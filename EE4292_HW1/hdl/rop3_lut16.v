e383c97ec461c756c23f05f51201886208894996
howard881010 <79572143+howard881010@users.noreply.github.com>
2022-09-24 10:49:56 +0800

    Add files via upload

---
+++ EE4292_HW1/hdl/rop3_lut16.v
@@ -0,0 +1,25 @@
+/*
+* Module      : rop3_lut16
+* Description : Implement this module using the look-up table (LUT) 
+*               This module should support all the 15-modes listed in table-1
+*               For modes not in the table-1, set the Result to 0
+* Notes       : Please remember to
+*               (1) make the bit-length of {P, S, D, Result} parameterizable
+*               (2) make the input/output to be a register 
+*/
+
+module rop3_lut16
+#(
+  parameter N = 4
+)
+(
+  input clk,
+  input [N-1:0] P,
+  input [N-1:0] S,
+  input [N-1:0] D,
+  input [7:0] Mode,
+  output reg [N-1:0] Result
+);
+
+
+endmodule
---
+++ EE4292_HW1/hdl/rop3_smart.v
@@ -0,0 +1,25 @@
+/*
+* Module      : rop3_smart
+* Description : Implement this module using the bit-hack technique mentioned in the assignment handout.
+*               This module should support all the possible modes of ROP3.
+* Notes       : Please remember to
+*               (1) make the bit-length of {P, S, D, Result} parameterizable
+*               (2) make the input/output to be a register
+*/
+
+module rop3_smart
+#(
+  parameter N = 4
+)
+(
+  input clk,
+  input [N-1:0] P,
+  input [N-1:0] S,
+  input [N-1:0] D,
+  input [7:0] Mode,
+  output reg [N-1:0] Result
+);
+
+
+
+endmodule
